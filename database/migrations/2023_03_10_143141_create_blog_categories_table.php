7a1c650e272d76e1748e937304faf5b7ab9b7342
Denisov <regkeys@inbox.ru>
2023-03-10 19:15:24 +0400

    master - create blog_categories and blog_posts migration

---
+++ database/migrations/2023_03_10_143141_create_blog_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('blog_categories', static function (Blueprint $table) {
+            $table->id();
+            $table->integer('parent_id')->unsigned()->default(0);
+
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('description')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog_categories');
+    }
+}
